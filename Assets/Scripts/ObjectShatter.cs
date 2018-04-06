ee378161be58ee8d78b67474965ff1e820a00c03
Rajat Gupta <rajat.gupta@ves.ac.in>
2018-04-05 22:04:50 -0400

    Initial commit

---
+++ Assets/Scripts/ObjectShatter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectShatter : MonoBehaviour {
+
+    private bool hasCollided = false;
+    public GameObject current, shatter;
+
+    private GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && !hasCollided)
+        {
+            hasCollided = true;
+            StartCoroutine(player.GetComponent<PlayerController>().DestroyObject(current, shatter));
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    [SerializeField]
+    private float radius = 1.0F;
+    [SerializeField]
+    private float power = 10.0F;
+
+    // Component caching
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        //if (PauseMenu.IsOn)
+        //{
+        //    if (Cursor.lockState != CursorLockMode.None)
+        //        Cursor.lockState = CursorLockMode.None;
+
+        //    motor.Move(Vector3.zero);
+        //    motor.Rotate(Vector3.zero);
+        //    motor.RotateCamera(0f);
+
+        //    return;
+        //}
+
+        //if (Cursor.lockState != CursorLockMode.Locked)
+        //{
+        //    Cursor.lockState = CursorLockMode.Locked;
+        //}
+
+        //Calculate movement velocity as a 3D vector
+        float _xMov = Input.GetAxis("Horizontal");
+        float _zMov = Input.GetAxis("Vertical");
+
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+
+        // Final movement vector
+        Vector3 _velocity = (_movHorizontal + _movVertical) * speed;
+
+    
+
+        //Apply movement
+        motor.Move(_velocity);
+
+        //Calculate rotation as a 3D vector (turning around)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(_rotation);
+
+        //Calculate camera rotation as a 3D vector (turning around)
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        float _cameraRotationX = _xRot * lookSensitivity;
+
+        //Apply camera rotation
+        motor.RotateCamera(_cameraRotationX);
+
+    }
+
+    public IEnumerator DestroyObject(GameObject current, GameObject shatter)
+    {
+        current.SetActive(false);
+        shatter.SetActive(true);
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null && rb.gameObject.tag != "Player")
+            {
+                rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+                //Debug.Log("Force Added");
+                rb.useGravity = true;
+            }
+        }
+        yield return null;
+    }
+
+}
